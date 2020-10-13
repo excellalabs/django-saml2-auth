0e3cda08170d953d6064ca9538f0b2aa2729132d
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2020-10-13 17:04:03 -0400

    removes extra space

--- django_saml2_auth/views.py
+++ django_saml2_auth/views.py
@@ -111,7 +111,6 @@ def _get_saml_client(domain):
     if 'NAME_ID_FORMAT' in settings.SAML2_AUTH:
         saml_settings['service']['sp']['name_id_format'] = settings.SAML2_AUTH['NAME_ID_FORMAT']
     
-
     spConfig = Saml2Config()
     spConfig.load(saml_settings)
     spConfig.allow_unknown_attributes = True
